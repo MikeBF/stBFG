d06f63cd05e99889afeb2ce0ec097866075fe604
MikeBF <138023211+MikeBF@users.noreply.github.com>
2024-04-18 18:30:25 +0300

    Update deployContracts.js

--- stBFGLocker/scripts/deployContracts.js
+++ stBFGLocker/scripts/deployContracts.js
@@ -1,25 +1,24 @@
 const { ethers, network } = require(`hardhat`);
 
-const treasuryAddress = '0xBb46693eBbEa1aC2070E59B4D043b47e2e095f86'
-const teamWalletAddress = '0xBb46693eBbEa1aC2070E59B4D043b47e2e095f86'
-const burnAddress = '0xBb46693eBbEa1aC2070E59B4D043b47e2e095f86'
+const treasuryAddress = '0x359b9807F9F823A6C2a8AF5073b154085a156fe6'
+const teamWalletAddress = '0xDb3B8B4542d1Fd25816B576B507BEa05B6488264'
 
 const main = async () => {
     let accounts = await ethers.getSigners()
     let deployer = accounts[0]
     let nonce = await network.provider.send('eth_getTransactionCount', [deployer.address, 'latest']) - 1
-    console.log(`Start deploing contracts. Deployer: ${deployer.address}`)
+    console.log(`Start deploying contracts. Deployer: ${deployer.address}`)
 
-    const deployParamsStBFGLocker = [treasuryAddress, teamWalletAddress, burnAddress]
+    const deployParamsStBFGLocker = [treasuryAddress, teamWalletAddress]
     const StBFGLockerFactory = await ethers.getContractFactory('StBFGLocker', deployer)
     const stBFGLocker = await StBFGLockerFactory.deploy(...deployParamsStBFGLocker, { nonce: ++nonce, gasLimit: 1e7})
     await stBFGLocker.deployed()
     console.log(`StBFGLocker contract deployed to ${stBFGLocker.address}`)
 
-    const LockBFGFactory = await ethers.getContractFactory('LockBFG', deployer)
-    const lockBFG = await LockBFGFactory.deploy(treasuryAddress, { nonce: ++nonce, gasLimit: 1e7})
-    await lockBFG.deployed()
-    console.log(`lockBFG contract deployed to ${lockBFG.address}`)
+    // const LockBFGFactory = await ethers.getContractFactory('LockBFG', deployer)
+    // const lockBFG = await LockBFGFactory.deploy(treasuryAddress, { nonce: ++nonce, gasLimit: 1e7})
+    // await lockBFG.deployed()
+    // console.log(`lockBFG contract deployed to ${lockBFG.address}`)
 
 }
 
