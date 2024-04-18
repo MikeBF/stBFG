97542006ecad4d3255dccd5619aeb3d834446a57
MikeBF <138023211+MikeBF@users.noreply.github.com>
2024-04-18 18:32:01 +0300

    Update IERC20.sol

--- stBFGLocker/contracts/IERC20.sol
+++ stBFGLocker/contracts/IERC20.sol
@@ -5,4 +5,5 @@ interface IERC20 {
     function balanceOf(address account) external view returns (uint256);
     function transfer(address to, uint256 value) external returns (bool);
     function transferFrom(address from, address to, uint256 value) external returns (bool);
+    function burn(uint amount) external returns (bool);
 }
